61a12557c99e62cc09f7df22317b42f9acf65119
Rene Roost <roost.rene@gmail.com>
2019-10-29 18:54:56 +0200

    Skoopide uurimine: singleton ja prototype skoobi võrdlemine luues objekte ning neid uurides

---
+++ src/ee/sport/SkoobiTestimiseApp.java
@@ -0,0 +1,31 @@
+package ee.sport;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SkoobiTestimiseApp {
+
+	public static void main(String[] args) {
+
+		ClassPathXmlApplicationContext kontekst = 
+				new ClassPathXmlApplicationContext("rakenduseKontekst.xml");
+		
+		Treener alfaTreener = kontekst.getBean("seeLobusTreener", Treener.class);
+		Treener beetaTreener = kontekst.getBean("seeLobusTreener", Treener.class);
+		
+		Treener gammaTreener = kontekst.getBean("sulgpalliTreener", Treener.class);
+		Treener deltaTreener = kontekst.getBean("sulgpalliTreener", Treener.class);
+		
+		boolean vastus = alfaTreener == beetaTreener;
+		System.out.println("alfaTreener ja beetaTreener sama objekti viidad: " + vastus);
+		System.out.println("alfaTreener asukoht mälus: " + alfaTreener);
+		System.out.println("beetaTreener asukoht mälus: " + beetaTreener + "\n");
+		
+		boolean vastus2 = gammaTreener == deltaTreener;
+		System.out.println("gammaTreener ja deltaTreener sama objekti viidad: " + vastus2);
+		System.out.println("gammaTreener asukoht mälus: " + alfaTreener);
+		System.out.println("deltaTreener asukoht mälus: " + deltaTreener + "\n");
+		
+		kontekst.close();
+	}
+
+}
--- src/ee/sport/SulgpalliTreener.java
+++ src/ee/sport/SulgpalliTreener.java
@@ -2,9 +2,11 @@ package ee.sport;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class SulgpalliTreener implements Treener {
 	
 	@Qualifier("positiivneEttekuulutusTeenus")
